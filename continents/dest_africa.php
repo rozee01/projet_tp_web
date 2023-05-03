bf4087976a6a48295dd098bea4ea9dd54c8fd043
Sabaa-Abdennabi <saba.abdennebi@insat.ucar.tn>
2023-05-03 16:09:54 +0200

    "finished continents"

--- continents/dest_africa.php
+++ continents/dest_africa.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="styles.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 
 <body>
