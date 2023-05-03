6d8461a28dfdbef7d91bad3aa270d42ffb643c01
jihenmkhinini <jihenemkhinini@insat.ucar.tn>
2023-05-03 16:16:07 +0200

    partie confirmer login+autoloader

--- Login.php
+++ Login.php
@@ -1,36 +1,36 @@
+<?php
+
+include_once './create/access.php'
+?>
+
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>TourBuddy Login</title>
-    <link rel="stylesheet" type="text/css" href="Login.css" />
-    <?php include_once('dependancies.php')?>
-  </head>
-  <body>
-    <?php include_once('header.php')?>
-    <div class="login-box">
-      <img src="pics/output-onlinepngtools.png" class="avatar" />
-      <h1>Sign In</h1>
-      <form>
-        <label for="username">Username or Email</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          placeholder="Enter your username or email"
-        />
-        <label for="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Enter your password"
-        />
-        <input type="submit" value="Log in" />
-      </form>
-      <p>don't have an account? Create one !<br> <a href="create/create.html">create a new account</a></p>
 
-    </div>
-    <?php include_once('footer.php')?>
-    <script src="home_demo.js"></script>
-  </body>
+<head>
+  <title>TourBuddy Login</title>
+  <link rel="stylesheet" type="text/css" href="Login.css" />
+  <?php include_once('dependancies.php') ?>
+</head>
+
+<body>
+  <?php include_once('header.php') ?>
+  <div class="login-box">
+    <img src="pics/output-onlinepngtools.png" class="avatar" />
+    <h1>Sign In</h1>
+    <form method="post">>
+      <label for="username">Username or Email</label>
+      <input type="text" id="username" name="username" placeholder="Enter your username or email" />
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password" placeholder="Enter your password" />
+      
+      <input type="submit" value="Log in" name="envoi" />
+    </form>
+    <p>don't have an account? Create one !<br> <a href="create/create.html">create a new account</a></p>
+
+  </div>
+  <?php include_once('footer.php') ?>
+  <!--<script src="home_demo.js"></script> -->
+</body>
+
 </html>
--- header.php
+++ header.php
@@ -26,7 +26,7 @@
             <a class="nav-link" href="../about_us/aboutus.php">About Us</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="Login.html" id="login">Log In</a>
+          <a class="nav-link" href="Login.php" id="login">Log In</a>
         </li>  
       </li>
       
