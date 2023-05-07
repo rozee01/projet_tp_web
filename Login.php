f2f3f2ede9dda7d0a35033a2a38981c7792da795
Sabaa-Abdennabi <saba.abdennebi@insat.ucar.tn>
2023-05-07 11:15:12 +0200

    "added session part"

--- Login.php
+++ Login.php
@@ -1,6 +1,13 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
+if (isset($_SESSION['user'])) {
+  header('location:index.php');
 
-include_once './create/access.php'
+}
+include_once './create/access.php';
+
 ?>
 
 
@@ -19,14 +26,14 @@ include_once './create/access.php'
     <img src="pics/output-onlinepngtools.png" class="avatar" />
     <h1>Sign In</h1>
     <form method="post">
-      <label for="username">Username or Email</label>
+      <label for="username">Username </label>
       <input type="text" id="username" name="username" placeholder="Enter your username or email" />
       <label for="password">Password</label>
       <input type="password" id="password" name="password" placeholder="Enter your password" />
       
       <input type="submit" value="Log in" name="envoi" />
     </form>
-    <p>don't have an account? Create one !<br> <a href="create/create.html">create a new account</a></p>
+    <p>don't have an account? Create one !<br> <a href="create/create.php">create a new account</a></p>
 
   </div>
   <?php include_once('footer.php') ?>
