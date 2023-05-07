4a92ceb0821d2fdb65283eeab799d7aaa16dbcaf
jihenmkhinini <jihenemkhinini@insat.ucar.tn>
2023-05-07 20:06:55 +0200

    completer_pays_europe+qlqueChangement

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3" id="navbar">
   <div class="container-fluid">
-    <img src= "../pics/tour.png"  style="width:140px;">
+    <img src="../pics/tour.png" style="width:140px;">
     <a class="navbar-brand" href="../index.php" id="logo" style="font-family: 'Rubik', sans-serif;">TourBuddy</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
@@ -9,7 +9,7 @@
       <div class="mx-auto">
         <ul class="navbar-nav">
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown">Destinations</a>
+            <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown">Destinations</a>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="continents/dest_africa.php">Africa</a></li>
               <li><a class="dropdown-item" href="continents/dest_as.php">Asia</a></li>
@@ -25,10 +25,13 @@
           <li class="nav-item">
             <a class="nav-link" href="../about_us/aboutus.php">About Us</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../Login.php" id="login">Log In</a>
-          </li>
-          </li>
+          <li class="nav-item dropdown" >
+            <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown">Compte</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="Login.php">Log In</a></li>
+              <li><a class="dropdown-item" href="create/create.php">Sign In</a></li>
+            </ul>
+          </li>     
 
         </ul>
       </div>
@@ -40,7 +43,7 @@
   var nav = document.querySelector("nav");
   window.addEventListener("scroll", function() {
     if (window.pageYOffset > 100) {
-      nav.classList.add("bg-light","shadow");
+      nav.classList.add("bg-light", "shadow");
     } else {
       nav.classList.remove("bg-light", "shadow");
     }
---
+++ insertion_paysdeeurope.sql
@@ -0,0 +1,56 @@
+INSERT INTO `pays` (`id`, `name`, `paragraph`, `place1`, `titlePlace1`, `place2`, `titlePlace2`, `place3`, `titlePlace3`, `place4`, `titlePlace4`, `place5`, `titlePlace5`, `place6`, `titlePlace6`, `food1`, `titleFood1`, `food2`, `titleFood2`, `food3`, `titleFood3`, `food4`, `titleFood4`, `food5`, `titleFood5`, `video`, `image`) VALUES
+(20, 'France', 'France, a land of exquisite beauty, rich history, and unparalleled charm, awaits your arrival. From the romantic streets of Paris to the sun-kissed beaches of the French Riviera, this enchanting country offers a myriad of experiences that will captivate your senses and leave you longing for more.
+
+Immerse yourself in the artistic masterpieces of the Louvre Museum, where the Mona Lisa beckons with her enigmatic smile. Explore the majestic châteaux of the Loire Valley, stepping into a world of fairy tales and grandeur. Indulge in the culinary delights of French cuisine, savoring delicate pastries, velvety cheeses, and world-renowned wines that will delight your taste buds.
+
+Venture beyond the iconic landmarks and discover the hidden gems that lie in every corner of France. Wander through quaint villages adorned with colorful flower boxes, where time seems to stand still. Stroll along the banks of the Seine River, absorbing the poetic ambiance that inspired countless writers and artists throughout history..',
+ 
+  'https://travellersworldwide.com/wp-content/uploads/2022/06/shutterstock_667548661.png.webp','Paris', 
+   'https://static1.s123-cdn-static-a.com/uploads/3086630/2000_5e4bcee616b92.jpg', 'Versailles',
+   'https://cdn.sortiraparis.com/images/80/1467/523194-visuel-paris-arc-de-triomphe.jpg','arc de triomphe', 
+   'https://a.travel-assets.com/findyours-php/viewfinder/images/res70/481000/481844-Bordeaux.jpg','Bordeaux', 
+   'https://rivierabarcrawltours.com/wp-content/uploads/2020/04/provence-pic.jpg', 'Provence', 
+    'https://www.planetware.com/photos-large/F/france-french-alps-chamonix.jpg', 'French Alps', 
+
+    'https://lacuisinedegeraldine.fr/wp-content/uploads/2023/01/homemade-croissants-26-scaled.jpg', 'Croissant', 
+     'https://foragerchef.com/wp-content/uploads/2014/02/foie-gras-with-ramps-and-apricots-4-2.jpg',  'Foie Gras', 
+     'https://www.papillesetpupilles.fr/wp-content/uploads/2015/08/Ratatouille-%C2%A9comeirrez-shutterstock.jpg', 'Ratatouille', 
+     'https://ileauxepices.com/blog/wp-content/uploads/2015/01/bouillabaisse-marsellaise-rouille-et-poisson.jpg',  'Bouillabaisse', 
+     'https://images-prod.healthline.com/hlcmsresource/images/AN_images/healthiest-cheese-1296x728-swiss.jpg','Cheese',
+
+      'https://www.youtube.com/embed/j9q3NLqUvxI',
+
+      'https://www.lonelyplanet.fr/sites/lonelyplanet/files/styles/manual_crop/public/media/destination/slider/mobile/paris2.jpg?itok=Apn99YoY'),
+
+(22, 'Spain', 'Spain, a land of vibrant culture and breathtaking beauty, beckons you to embark on an unforgettable journey. From the lively streets of Barcelona to the historic charm of Madrid, Spain offers a tapestry of experiences that will leave you enchanted. Indulge in the rich flavors of tapas, savor the world-renowned wines, and immerse yourself in the passionate rhythms of flamenco. Explore the architectural wonders of Antoni Gaudí, lose yourself in the narrow alleys of ancient towns, or bask in the sun-kissed beaches along the Mediterranean coast. Whether you seek art, history, cuisine, or simply the warmth of its people, Spain will captivate your heart and ignite your senses. Let the spirit of fiesta embrace you as you discover the magic and charm that make Spain an irresistible destination. Come, let Spain embrace you in its embrace of beauty and culture.',
+ 'https://cdn.generationvoyage.fr/2019/05/visiter-ibiza-e1558965384643.jpg', 'Ibiza',
+  'https://tinyurl.com/5fbjdua2', 'Barcelona',
+   'https://www.hellehollis.com/blog/en/wp-content/uploads/2019/08/Cordoba-at-sunset.jpg', 'Cordoba',
+    'https://a.cdn-hotels.com/gdcs/production85/d1474/95ef4e66-fd92-41dc-99af-c7f628857eb6.jpg', 'Seville',
+     'https://www.caminodesantiago.gal/osdam/filestore/7/1/7/6/0_7eef4e67c0cf83d/71760scr_0015f5e21ee4627.jpg', 'Santiago de Compostela',
+      'https://static.saltinourhair.com/wp-content/uploads/2019/07/23133327/valencia-spain-city-arts-sciences.jpg', 'Valencia', 
+
+      'https://content.joseedistasio.ca/app/uploads/2020/03/04102344/PAELLA.jpg', 'Paella', 
+      'https://images.immediate.co.uk/production/volatile/sites/30/2022/05/Meatball-Patatas-Bravas-2dcb0fc.jpg?quality=90&resize=556,505', 'Tapas',
+       'https://spanishsabores.com/wp-content/uploads/2015/11/IMG_5988-1024x683.jpg', 'Pintxos', 
+       'https://resize.elle.fr/portrait/var/plain_site/storage/images/elle-a-table/recettes-de-cuisine/churros-ou-chichis-2078960/22073372-3-fre-FR/Churros-ou-chichis.jpg', 'Churros ',
+        'https://assets.unileversolutions.com/recipes-v2/37805.jpg', 'Tortilla Española', 
+        'https://www.youtube.com/embed/n2ZvJd8aDRU',
+        'https://www.schengenvisainfo.com/news/wp-content/uploads/2022/02/Barcelona-Spain.-Nighttime-top-view-at-Spanish-Square.jpg'),
+
+(21, 'Italy', 'Spain, Italy, a captivating country that weaves history, art, and natural beauty into an enchanting tapestry, awaits your discovery. From the ancient ruins of Rome to the romantic canals of Venice, Italy offers a sensory feast that will leave you spellbound. Immerse yourself in the Renaissance wonders of Florence, where masterpieces by Michelangelo and Botticelli adorn magnificent galleries. Explore the picturesque villages of the Amalfi Coast, bask in the glorious Tuscan countryside, or indulge in the culinary delights of Naples and Bologna. Let the serene beauty of Lake Como and the vibrant colors of the Cinque Terre take your breath away. Whether you seek cultural treasures, breathtaking landscapes, or delectable cuisine, Italy is a feast for the senses. Come, wander through the ancient ruins, stroll along cobblestone streets, and savor the warmth and hospitality of the Italian people. Let Italy\'s timeless allure capture your heart and create memories to last a lifetime.',
+ 'https://www.fodors.com/wp-content/uploads/2018/10/HERO_UltimateRome_Hero_shutterstock789412159.jpg', 'Rome',
+  'https://cdn.britannica.com/71/8671-050-2EE6A745/Cathedral-Florence-Santa-Maria-del-Fiore.jpg', 'Florence',
+   'https://static.saltinourhair.com/wp-content/uploads/2019/04/23140026/things-to-do-venice-italy-header-768x662.jpg', 'Venice',
+    'https://www.travelandleisure.com/thmb/c-nuaEqUffHAByhV7Ls6iDEeXwc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-header-atrani-italy-amalfi-coast-AMALFITOWNS0223-c516bc91bb434e19b5ec6e2fb50cb9eb.jpg', 'Amalfi Coast',
+     'https://partir.ouest-france.fr/magazine/wp-content/uploads/2019/09/cinque-terre.jpg', 'Cinque Terre',
+      'https://media.timeout.com/images/105490133/image.jpg', 'Sicily', 
+
+      'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/62c59f6365a259b03da440de3973f201/Derivates/668cba6648888c61d249c0a5d9651157a4ce3793.jpg', 'Pizza Napoletana', 
+      'https://realfood.tesco.com/media/images/1400x919-SpaghettiCarbonara-557b6ff5-c4f3-4565-ae8e-a506f7dcc415-0-1400x919.jpg', 'Pasta Carbonara:',
+       'https://insanelygoodrecipes.com/wp-content/uploads/2022/07/Assorted-Gelato-Ice-Cream-with-Berries-Chocolate-and-Pistachio-500x500.jpg', 'Gelato', 
+       'https://www.mycake.fr/wp-content/uploads/2020/11/rs_tiramisu_1x1.jpg', 'Tiramisu',
+        'https://www.pastafrescarossi.it/Handlers/Image.ashx?ID=82162673-7781-4417-bfeb-c74ae078b5e4&src=ravioli-di-magro-860x530.jpg&wh=%40%1DS', 'Ravioli di Magro', 
+        
+        'https://www.youtube.com/embed/avLutMvjrQ0', 
+        'https://lp-cms-production.imgix.net/features/2017/11/GettyRF_543346423-1-ab159824d5bd.jpg');
