83d0a6635b2fcf9baf4cf838dada039111054473
Nouira Taher <tahernouira003@gmail.com>
2023-05-07 10:33:59 +0200

    continents wallet fichier bark php , lbe9i fl data base , added country suggestion ( ne9es juste l fichier mta3 l country mteeou )

---
+++ continents/Suggestion.php
@@ -0,0 +1,168 @@
+
+<style>
+    .container.form{
+	position: relative;
+    margin-top : 100px ;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 20px 100px;
+}
+
+.contact-box{
+	max-width: 850px;
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	background-color: #fff;
+	box-shadow: 0px 0px 19px 5px rgba(0,0,0,0.19);
+}
+
+.left {
+	position: relative;
+	height: 100%;
+}
+
+.left::before {
+	content: "";
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: url("") no-repeat center;
+	background-size: cover;
+}
+
+#image-preview {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	width: 100%;
+	height: 100%;
+}
+
+#image-upload {
+	position: absolute;
+	top: calc(50% + 50px);
+	left: 50%;
+	transform: translateX(-50%);
+}
+
+.btn-left {
+	position: absolute;
+	top: calc(50% - 20px);
+	left: 50%;
+	transform: translateX(-50%);
+    color: #fff;
+    background-color: #ab1b3f;
+
+}
+
+
+.right{
+	padding: 25px 40px;
+}
+
+h2{
+	position: relative;
+	padding: 0 0 10px;
+	margin-bottom: 10px;
+}
+
+h2:after{
+	content: '';
+    position: absolute;
+    left: 50%;
+    bottom: 0;
+    transform: translateX(-50%);
+    height: 4px;
+    width: 50px;
+    border-radius: 2px;
+    background-color: #ab1b3f;
+}
+
+.field{
+	width: 100%;
+	border: 2px solid rgba(0, 0, 0, 0);
+	outline: none;
+	background-color: rgba(230, 230, 230, 0.6);
+	padding: 0.5rem 1rem;
+	font-size: 1.1rem;
+	margin-bottom: 22px;
+	transition: .3s;
+}
+
+.field:hover{
+	background-color: rgba(0, 0, 0, 0.1);
+}
+
+textarea{
+	min-height: 150px;
+}
+
+.btn-right{
+	width: 100%;
+	padding: 0.5rem 1rem;
+	background-color: #ab1b3f;
+	color: #fff;
+	font-size: 1.1rem;
+	border: none;
+	outline: none;
+	cursor: pointer;
+	transition: .3s;
+}
+
+.btn:hover{
+    background-color: #f49696 ;
+}
+
+.field:focus{
+    border: 2px solid rgba(30,85,250,0.47);
+    background-color: #fff;
+}
+
+
+</style>
+<div class="container form">
+<form action="AddCountryProcess.php" method="POST" enctype="multipart/form-data">
+		<div class="contact-box my-form">
+			<div class="left">
+				<img id="image-preview" src="" alt="Preview Image" width="100%">
+				<input type="file" name="image" id="image-upload" style="display:none">
+				<button type="button" class="btn  btn-left" onclick="document.getElementById('image-upload').click()">Select Image</button>
+			</div>
+			<div class="right">
+				<h2>Suggest a country ! </h2>
+                <input type="hidden" name="continent" value="<?php echo $continentName; ?>">
+				<input type="text" class="field" placeholder="Name of the country" name="name">
+				<textarea placeholder="Add some description" class="field" name="description"></textarea>
+				 <button class="btn btn-right" id="send-btn">Send</button>
+			</div>
+		</div>
+</form>
+</div>
+<script>
+document.getElementById('image-upload').addEventListener('change', function() {
+	var reader = new FileReader();
+	reader.onload = function(e) {
+		document.getElementById('image-preview').setAttribute('src', e.target.result);
+	}
+	reader.readAsDataURL(this.files[0]);
+});
+
+document.getElementById('send-btn').addEventListener('click', function() {
+	document.getElementsByTagName('form')[0].submit();
+});
+function showForm() {
+    var formContainer = document.getElementById("form-container");
+    formContainer.style.display = "block";
+    
+}
+</script>
+    
---
+++ continents/continents.php
@@ -0,0 +1,83 @@
+<?php 
+$continentName=$_GET['name'];
+include_once('../CountriesRepository.php')
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $continentName ?></title>
+    <?php include_once('../dependancies.php') ?>
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include_once('../continents/header2.php') ?>
+
+    <!--top destinations start-->
+
+    <div class="container">
+        <h1 class="text-center mt-5">Top Destinations to explore in <?= $continentName ?> </h1>
+        <div class="row">
+        <?php
+    $Continent= new CountriesRepository();
+    $countries = $Continent->findAll(['continent' => $continentName]);
+    foreach ($countries as $country) {
+    $country_elements = ['continent',
+        'name', 'description', 'image'];
+    foreach ($country_elements as $element) {
+        ${$element} = $country->{$element};
+    }
+?>
+       
+            <div class="col-lg-4 col-md-6">
+                <div class="destination-box">
+                    <div class="card">
+                        <img src=<?= $image ?> class="img-fluid">
+                        <div class="descriptions bg-light">
+                            <h1><?= $name ?></h1>
+                            <p>
+                                    <?= $description ?>
+                                 </p>
+
+                            <button style="margin-left: 10%;">
+                                <i class="fab fa-youtube"></i>
+                                <a class="nav-link" style="text-decoration:none; color: light; " href=<?php $reference = "../pays/pays.php?name=".$name ; echo $reference ; ?> > Know more about <?php if($name =='The Great Barrier Reef') {echo 'this country'; }else {echo $name ;}?></a>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+         
+            <?php } ;
+             include_once('Suggestion.php')  ;?>
+            
+            
+            </div>
+            
+        <button type="button" class="lien" >
+            <i class="fab fa-youtube"></i>
+            <a class="nav-item nav-link" style="text-decoration:none; color: light; " href="../index.php">
+            Back to the continents 
+            </a>
+        </button>
+   
+        </div>
+    
+    <!--top destionations end-->
+
+    <?php include_once('../footer.php') ?>
+
+    <!-- Include Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
