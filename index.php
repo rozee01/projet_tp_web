e6b9445b07ccb2074821a39ec1e4724c147ede20
Arij Thabet <arijthabet01@gmail.com>
2023-05-10 14:28:32 +0100

    sallaht path l header

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!-- hedha comment zyeda zeyda -->
 <head>
 <?php $path="./";
-	
+	// sallaht l path
 	?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
