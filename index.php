72f191e5a1396886091b536f910ee69aa1653a89
Arij Thabet <109590886+rozee01@users.noreply.github.com>
2023-05-10 14:30:52 +0100

    Update index.php
    baddalt l path lahnee

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 <html lang="en">
 
 <head>
-<?php $pathindex="./";
-	$patht="./";$pathab="./";$pathlogin="./";$pathsignin="./";
+<?php	$path="./";
 	?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
